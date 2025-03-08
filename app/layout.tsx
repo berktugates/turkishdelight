bbd19cb9659f4e80052256d954002b1b927b151b
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-03-08 05:01:33 +0300

    feat:add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeContext } from "@/store/context/ThemeContext";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <ThemeContext>
           {children}
           <Analytics />
+          <SpeedInsights />
         </ThemeContext>
       </body>
     </html>
